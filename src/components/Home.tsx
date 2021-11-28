d319946d7791968a8fb8065c01f9a12854cf4c4d
ranemihir <mihirrane171@gmail.com>
2021-11-28 23:31:18 +0530

    minor fixes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,10 +9,10 @@ export const Home = (props: { currentUser: User, todos: string[]; }) => {
     const [todo, setTodo] = useState<string>('');
     const [todos, setTodos] = useState<string[]>(props.todos);
 
-    const handleSubmit = async (event: any) => {
+    const handleClick = () => {
         setTodos((todosState) => todosState.concat(todo));
+        console.log(todos);
         setTodo('');
-        event.preventDefault();
     };
 
     const handleChange = (event: any) => {
@@ -30,7 +30,7 @@ export const Home = (props: { currentUser: User, todos: string[]; }) => {
                             <img src={currentUser.photoUrl} className="img-thumbnail rounded-circle shadow-sm me-2" alt={currentUser.firstName + ' thumbnail'} width='48' />
                             <div className="d-flex flex-column">
                                 <div>{currentUser.firstName + ' ' + currentUser.lastName}</div>
-                                <a className="text-muted" href='/api/auth/google/logout'>Sign Out</a>
+                                <a className="text-muted text-decoration-none" href='/api/auth/google/logout'><small>Sign Out</small></a>
                             </div>
                         </div>
                     </div>
@@ -38,12 +38,12 @@ export const Home = (props: { currentUser: User, todos: string[]; }) => {
             </nav>
             <div className="container">
                 <div className="row justify-content-center pt-5">
-                    <form className="d-flex mt-5 mx-3" onSubmit={handleSubmit}>
+                    <div className="d-flex mt-5 mx-3">
                         <input onChange={handleChange} value={todo} className="form-control me-2 border-secondary" type="search" placeholder="Write Something..." aria-label="Search" style={{ fontWeight: 500, letterSpacing: 1 }} />
-                        <button className="btn btn-outline-primary" type="submit" style={{ fontWeight: 500, letterSpacing: 1 }}>
+                        <button className="btn btn-outline-primary" style={{ fontWeight: 500, letterSpacing: 1 }} onClick={handleClick}>
                             Add
                         </button>
-                    </form>
+                    </div>
                 </div>
                 <div className="row justify-content-center pt-5">
                     {
